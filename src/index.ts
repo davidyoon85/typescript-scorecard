b0e2f00c095437000d91b597eadb00a14fcf4bb8
David Yoon <davidyoon85@gmail.com>
2021-03-05 19:53:54 -0500

    add csv file, read file and prase file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log("hello!");
+import fs from "fs";
+
+const matches = fs
+    .readFileSync("football.csv", {
+        encoding: "utf-8"
+    })
+    .split("\n")
+    .map((row: string): string[] => {
+        return row.split(",");
+    });
+
+let manUnitedWins = 0;
+
+for (let match of matches) {
+    let manUnitedWinsAtHome = match[1] === "Man United" && match[5] === "H";
+    let manUnitedWinAway = match[2] === "Man United" && match[5] === "a";
+    if (manUnitedWinsAtHome || manUnitedWinAway) {
+        manUnitedWins++;
+    }
+}
+
+console.log(`Man United won ${manUnitedWins} game`);
