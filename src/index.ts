7277b8538d53acb5e771b64e99ec53b5c65aa2af
David Yoon <davidyoon85@gmail.com>
2021-03-06 19:20:08 -0500

    abstract csv file read logic

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import fs from "fs";
+import { CsvFileReader } from "./CsvFileReader";
 
-const matches = fs
-    .readFileSync("football.csv", {
-        encoding: "utf-8"
-    })
-    .split("\n")
-    .map((row: string): string[] => {
-        return row.split(",");
-    });
+const reader = new CsvFileReader("football.csv");
+reader.read();
+
+enum MatchResults {
+    HomeWin = "H",
+    AwayWin = "A",
+    Draw = "D"
+}
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
-    let manUnitedWinsAtHome = match[1] === "Man United" && match[5] === "H";
-    let manUnitedWinAway = match[2] === "Man United" && match[5] === "a";
+for (let match of reader.data) {
+    let manUnitedWinsAtHome = match[1] === "Man United" && match[5] === MatchResults.HomeWin;
+    let manUnitedWinAway = match[2] === "Man United" && match[5] === MatchResults.AwayWin;
     if (manUnitedWinsAtHome || manUnitedWinAway) {
         manUnitedWins++;
     }
