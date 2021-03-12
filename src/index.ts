86b4714fce869b33337a17202a6d81aabd13139b
David Yoon <davidyoon85@gmail.com>
2021-03-11 21:26:34 -0500

    convert CsvFileReader to abstract class using generic

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from './MatchResult';
+import { MatchReader } from "./MatchReader";
+import { MatchResult } from "./MatchResult";
 
-const reader = new CsvFileReader("football.csv");
+const reader = new MatchReader("football.csv");
 reader.read();
 
 let manUnitedWins = 0;
 
 for (let match of reader.data) {
-    let manUnitedWinsAtHome = match[1] === "Man United" && match[5] === MatchResults.HomeWin;
-    let manUnitedWinAway = match[2] === "Man United" && match[5] === MatchResults.AwayWin;
+    let manUnitedWinsAtHome = match[1] === "Man United" && match[5] === MatchResult.HomeWin;
+    let manUnitedWinAway = match[2] === "Man United" && match[5] === MatchResult.AwayWin;
     if (manUnitedWinsAtHome || manUnitedWinAway) {
         manUnitedWins++;
     }
